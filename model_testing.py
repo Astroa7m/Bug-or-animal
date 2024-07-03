a93c4554884fda20853f11ab03fc9011dfc3b7e1
Ahmed Samir <ahmed123.as27@gmail.com>
2024-07-04 00:18:49 +0300

    modified confidence rate

--- model_testing.py
+++ model_testing.py
@@ -4,9 +4,11 @@ from fastai.vision.core import PILImage
 
 learn = load_learner("animal_or_bug_model.pkl")
 
-test_image = PILImage.create("animal.webp")
+test_image = PILImage.create("bug 2.jpg")
+
+print(learn.dls.vocab)
 
 pred, _, probs = learn.predict(test_image)
 
 print(f"Prediction: {pred}")
-print(f"Confidence: {probs[0]}")
+print(f"Confidence: {probs[0] if pred == "animal" else probs[1]}")
